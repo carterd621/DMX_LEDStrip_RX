d064b93de164acaa816d47f71284b4071be569e1
Dylan Carter <dylancarter@Dylans-MacBook-Pro-2.local>
2022-03-24 17:53:44 -0400

    adding main files and .hs to acompany

---
+++ 0main.ino
@@ -0,0 +1,274 @@
+#include <FastLED.h>
+#include <DMXSerial.h>
+#include <ArducamSSD1306.h>
+
+#include "masterDefs.h"
+#include "ledFuncs.h"
+
+int mode = 0; //default mode is pixel mapped mode
+//MODE DEFINITIONS
+//MODE 1: Pixel Mapped (150 channels)
+//MODE 2: RGB, FX select and FX speed (5 channels)
+
+int address = 1; //default address is 1 (can this be changed to not change when device powers off)
+
+//display setup
+#define OLED_RESET  16  // Pin 15 -RESET digital signal
+
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+
+ArducamSSD1306 display(OLED_RESET); // FOR I2C
+
+CRGB leds[NUM_LEDS]; //defines LED object
+
+const int SL = 0; //defines starting LED
+const int EL = NUM_LEDS - 1; //defines ending LED
+const int wait = (-1.0 / 25.0 * NUM_LEDS) + 7;
+
+const int startChannel = 100;
+
+//////////////////EFFECTS BUILDERS///////////////////
+void blackout() {
+  for (int i = SL; i <= EL; i++) {
+    leds[i] = CRGB::Black;
+  }
+  FastLED.show();
+}
+
+void fadeOut(int t, int h, int s, int v) {
+  int vc = v;  //define brightness changing as the LEDs dim
+  while (vc > 0) {
+    vc--;
+    for (int j = SL; j <= EL; j++)
+      leds[j] = CHSV(h, s, vc);
+    FastLED.show();
+    delay(double(t / v));
+  }
+}
+
+void fadeIn(int t, int h, int s, int v) { //fades to the RGB level across all inciated LEDs
+  int vc = 0;
+  while (vc < v) {
+    vc++;
+    for (int i = SL; i <= EL; i++)
+      leds[i] = CHSV(h, s, vc);
+    FastLED.show();
+    delay(double(t / v));
+  }
+}
+
+void fadeColor(double t, double h0, double h, int sat, int bright) { //fades out from RGB across indicated LEDs
+  double hstep = (h - h0) / (t / 10);
+  for (int j = SL; j <= EL; j++) {
+    leds[j] = CHSV(h0, sat, bright);
+  }
+  FastLED.show();
+  for (int i = 0; i < t; i += 10) {
+    h0 = h0 + hstep;
+    for (int k = SL; k < EL; k++)
+      leds[k] = CHSV(h0, sat, bright);
+    FastLED.show();
+    delay(10);
+  }
+}
+
+void runner(int r, int g, int b, int mode) { //mode1: forward //mode2: backward //mode3: mirror out //mode4: mirror in
+  if (mode == 1) {
+    for (int i = SL; i <= EL; i++) {
+      leds[i] = CRGB(b, r, g);
+      FastLED.show();
+      delay(2 * wait);
+    }
+  }
+  else if (mode == 2) {
+    for (int i = EL; i >= SL; i--) {
+      leds[i] = CRGB(b, r, g);
+      FastLED.show();
+      delay(2 * wait);
+    }
+  }
+  else if (mode == 3) {
+    for (int i = (EL - SL) / 2; i >= SL; i--) {
+      leds[i] = CRGB(b, r, g);
+      leds[EL - i] = CRGB(b, r, g);
+      FastLED.show();
+      delay(5 * wait);
+    }
+  }
+  else if (mode == 4) {
+    for (int i = SL; i <= ((EL - SL) / 2 + 1); i++) {
+      leds[i] = CRGB(b, r, g);
+      leds[EL - i] = CRGB(b, r, g);
+      FastLED.show();
+      delay(5 * wait);
+    }
+  }
+  blackout();
+}
+
+void strobe() { //change strobe to be specific
+  int r = DMXSerial.read(startChannel);
+  int g = DMXSerial.read(startChannel + 1);
+  int b = DMXSerial.read(startChannel + 2);
+  int t = 2 * DMXSerial.read(startChannel + 4);
+  //  for (int k = 0; k < 10; k++) {
+  for (int i = SL; i <= EL; i++)
+    leds[i] = CRGB(b, r, g);
+  FastLED.show();
+  delay(6);
+  for (int i = SL; i < EL; i++)
+    leds[i] = CRGB(0, 0, 0);
+  FastLED.show();
+  delay(t);
+  //  }
+}
+
+void blnk(int t) {
+  leds[0] = CRGB(0, 100, 0);
+  FastLED.show();
+  delay(t);
+  leds[0] = CRGB(0, 0, 0);
+  FastLED.show();
+  delay(t);
+}
+
+//////////////////EFFECTS///////////////////
+void solidRGB(int r, int g, int b) {
+  for (int i = SL; i <= EL; i++) {
+    leds[i] = CRGB(b, r, g); //BRG ordering
+  }
+  FastLED.show();
+}
+
+void solidHSV(int h, int s, int v) {
+  for (int i = SL; i <= EL; i++) {
+    leds[i] = CHSV(h, s, v);
+  }
+  FastLED.show();
+}
+
+void dj() {
+  int mode = random(0, 1000);
+  if (mode <= 150)
+    strobe();
+  else if ((mode <= 400) && (mode > 150)) {
+    int color1 = random(0, 13) * 10;
+    int color2 = random(14, 26) * 10;
+    int tFade = random(300, 1500);
+    fadeColor(tFade, color1, color2, 255, 255);
+  }
+  else if ((mode <= 700) && (mode > 400)) {
+    int color = random(0, 26) * 10;
+    solidHSV(color, 0, 150);
+    delay(random(0, 5) * 100);
+  }
+  else if (mode > 700) {
+    if (rand() % 2 == 0)blackout();
+    runner(random(0, 245), 255, 200, random(100, 500) / 100);
+  }
+}
+
+// MASTER FX LIST for 5 channel mode
+// DMX VALUES for CHANNEL 4 when in 5 Channel Mode
+// CHANNELS:
+// 0 ----- Solid Color
+// 1<49 -- Strobe (speed fast to slow)
+// 50<59 front to back 60<69 back to front 70<79 mirror out 80<89 mirror in (runner effects)
+
+void FXSel(int f) { //functions used for FX selection
+  if (f == 0 || f == 255)
+    solidRGB(DMXSerial.read(startChannel),
+             DMXSerial.read(startChannel + 1),
+             DMXSerial.read(startChannel + 2));
+  else if (f > 0 && f < 50)
+    strobe();
+  else if (f >= 50 && f < 90)
+    runner(DMXSerial.read(startChannel),
+           DMXSerial.read(startChannel + 1),
+           DMXSerial.read(startChannel + 2),
+           ((DMXSerial.read(startChannel + 3) - 50) / 10) + 1);
+}
+
+void pixelMap() {
+  for (int i = SL; i <= EL; i++) {
+    leds[i] = CRGB(DMXSerial.read(startChannel + 2 + (3 * i)),
+                   DMXSerial.read(startChannel + (3 * i)),
+                   DMXSerial.read(startChannel + 1 + (3 * i)));
+  }
+  FastLED.show();
+  delay(10); //more delay needed for pixel map mode
+}
+
+//screen functions
+void mainScreen(){
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("MIDI NO SUCCESS");
+    display.setCursor(0,20);
+    display.println("FUCKKKK");
+    display.display();
+}
+
+void errorScreen(){
+    ;
+}
+
+void modeScreen(){
+    ;
+}
+
+//button functions to be called by interrupts
+void modeSelect(){
+    if(mode==1)
+        mode = 0;
+    else
+        mode++;
+}
+
+void addressUp(){
+    if(address==512)
+        address = 1;
+    else
+        address++;
+}
+
+void addressDn(){
+    if(address==1)
+        address = 512;
+    else
+        address--;
+}
+
+//functions for the LED output based on modes
+void pixMap(){ //LED output based on DMX channel
+    ;
+}
+
+void fiveChannel(){
+    ;
+}
+
+void setup() {
+  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);  // BRG ordering is typical
+  DMXSerial.init(DMXReceiver);
+
+  pinMode(A7, INPUT);
+  blackout();
+  randomSeed(analogRead(7));
+
+  
+
+  display.begin();
+}
+
+void loop() {
+    if (mode==0)
+        pixMap();
+    else if(mode==1)
+        fiveChannel();
+    else
+        errorScreen();
+}
---
+++ masterDefs.h
@@ -0,0 +1,6 @@
+#define NUM_LEDS 50 //number of controller chips per strip
+#define DATA_PIN 12 //initializes pin for data
+#define ADRUP_PIN 4
+#define ADRDN_PIN 5
+#define MODE_PIN 6
+#define RAND_PIN A7
