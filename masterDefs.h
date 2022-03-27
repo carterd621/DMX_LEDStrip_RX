1afa4e91689798b0cfa9fc3275fd2e6885e3aeda
Dylan Carter <dylancarter@Dylans-MBP-2.attlocal.net>
2022-03-26 22:12:35 -0400

    move more information from main to defines document

--- masterDefs.h
+++ masterDefs.h
@@ -1,5 +1,9 @@
-#define NUM_LEDS 50 //number of controller chips per strip
-#define DATA_PIN 12 //initializes pin for data
+#define NUM_LEDS 50
+#define SL 0 //defines starting LED
+#define EL NUM_LEDS - 1 //defines ending LED
+#define WAIT_TIME (-1.0 / 25.0 * NUM_LEDS) + 7
+
+#define DATA_PIN 12
 #define ADRUP_PIN 4
 #define ADRDN_PIN 5
 #define MODE_PIN 6
